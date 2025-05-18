ad1430f1c12270f03a2172d99951d89e5dc6cd18
임재혁 <imsdm@naver.com>
2025-05-18 19:59:56 +0900

    test: 웹소켓 기능 각 server.js, backserver.js로 빼놓음
    메인서버에 웹소켓이 따로 움직여야해서 app.js내부도 수정은 필요했기에 backserver에도 그냥 따로 빼놓음, 근데 server.js는 왜 작동하는지 모르겠음

--- backserver.js
+++ backserver.js
@@ -1,8 +1,30 @@
 const express=require('express');
 const app = require('./app');
+const socketIo = require('socket.io');
+const http = require('http');
+
 const port=3001;
 
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3001", // 요청을 허용할 클라이언트 주소
+        methods: ["GET", "POST"]
+    }
+});
 
-app.listen(port, () => {
+app.set('io', io);
+
+io.on('connection', socket => {
+    console.log('클라이언트 접속됨');
+
+    socket.on('disconnect', () => {
+        console.log('클라이언트 연결 해제됨');
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log(`테스트 서버가 ${port} 실행됩니다.`);
 });
